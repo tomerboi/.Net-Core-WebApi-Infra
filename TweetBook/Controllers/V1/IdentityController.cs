abf36264996f1a43a08ac4c4cf000dd2b55972d6
tomerboi <tomerboyman@gmail.com>
2020-07-04 21:36:15 +0300

    add all

---
+++ TweetBook/Controllers/V1/IdentityController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TweetBook.Contracts;
+using TweetBook.Contracts.V1.Requests;
+using TweetBook.Contracts.V1.Response;
+using TweetBook.Services.Identity;
+
+namespace TweetBook.Controllers.V1
+{
+    public class IdentityController : Controller
+    {
+        private readonly IIdentityService _identityService;
+
+        public IdentityController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        [HttpPost(ApiRoutes.Identity.Register)]
+        public async Task<IActionResult> Register([FromBody] UserRegistrationRequest userRegistrationRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = ModelState.Values.SelectMany(x => x.Errors.Select(xx => xx.ErrorMessage))
+                });
+            }
+            var authResult = await _identityService.RegisterAsync(userRegistrationRequest.Email, userRegistrationRequest.Password);
+
+            if (!authResult.Success)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = authResult.ErrorMessages
+                });
+            }
+            return Ok(new AuthSuccessResponse
+            {
+                Token = authResult.Token,
+                RefreshToken = authResult.RefreshToken
+            });
+        }
+
+        [HttpPost(ApiRoutes.Identity.Login)]
+        public async Task<IActionResult> Login([FromBody] UserLoginRequest userLoginRequest)
+        {
+            var authResult = await _identityService.LoginAsync(userLoginRequest.Email, userLoginRequest.Password);
+
+            if (!authResult.Success)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = authResult.ErrorMessages
+                });
+            }
+
+            return Ok(new AuthSuccessResponse
+            {
+                Token = authResult.Token,
+                RefreshToken = authResult.RefreshToken
+            });
+        }
+
+        [HttpPost(ApiRoutes.Identity.Refresh)]
+        public async Task<IActionResult> Refresh([FromBody] RefreshTokenRequest userLoginRequest)
+        {
+            var authResult = await _identityService.RefreshTokenAsync(userLoginRequest.Token, userLoginRequest.RefreshToken);
+
+            if (!authResult.Success)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = authResult.ErrorMessages
+                });
+            }
+
+            return Ok(new AuthSuccessResponse
+            {
+                Token = authResult.Token,
+                RefreshToken = authResult.RefreshToken
+            });
+        }
+    }
+}
---
+++ TweetBook/Services/Identity/IdentityService.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TweetBook.Domain;
+using TweetBook.Options;
+
+namespace TweetBook.Services.Identity
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtSettings _jwtSettings;
+        private readonly TokenValidationParameters _tokenValidationParameters;
+
+        public IdentityService(UserManager<IdentityUser> userManager, JwtSettings jwtSettings, TokenValidationParameters tokenValidationParameters)
+        {
+            _userManager = userManager;
+            _jwtSettings = jwtSettings;
+            _tokenValidationParameters = tokenValidationParameters;
+        }
+
+        public async Task<AuthenticationResult> LoginAsync(string email, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                return new AuthenticationResult
+                {
+                    ErrorMessages = new[] { "User does not exist." }
+                };
+            }
+
+            var userHasValidPassword = await _userManager.CheckPasswordAsync(user, password);
+            if(!userHasValidPassword)
+            {
+                return new AuthenticationResult
+                {
+                    ErrorMessages = new[] { "User/pass combination is wrong." }
+                };
+            }
+
+            return GetAuthenticationResult(user);
+        }
+
+        public async Task<AuthenticationResult> RefreshTokenAsync(string token, string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private ClaimsPrincipal GetPrincipalFromToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            try
+            {
+                var principal = tokenHandler.ValidateToken(token, _tokenValidationParameters, out var securityToken);
+                if (!IsJwtWithValidAlgorithm(securityToken))
+                {
+                    return null;
+                }
+
+                return principal;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private bool IsJwtWithValidAlgorithm(SecurityToken validatedToken)
+        {
+            return (validatedToken is JwtSecurityToken jwtSecurityToken) &&
+                jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCulture);
+        }
+
+        public async Task<AuthenticationResult> RegisterAsync(string email, string password)
+        {
+            var existingUser = await _userManager.FindByEmailAsync(email);
+            if(existingUser != null)
+            {
+                return new AuthenticationResult
+                {
+                    ErrorMessages = new[] { "User with the same email already exist." }
+                };
+            }
+
+            var newUser = new IdentityUser
+            {
+                Email = email,
+                UserName = email
+            };
+
+            var createdUser = await _userManager.CreateAsync(newUser, password);
+
+            if (!createdUser.Succeeded)
+            {
+                return new AuthenticationResult
+                {
+                    ErrorMessages = createdUser.Errors.Select(x => x.Description)
+                };
+            }
+
+            return GetAuthenticationResult(newUser);
+        }
+
+        private AuthenticationResult GetAuthenticationResult(IdentityUser newUser)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, newUser.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Email, newUser.Email),
+                    new Claim("id", newUser.Id),
+                }),
+                Expires = DateTime.UtcNow.Add(_jwtSettings.TokenLifeTime),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return new AuthenticationResult
+            {
+                Success = true,
+                Token = tokenHandler.WriteToken(token)
+            };
+        }
+    }
+}
